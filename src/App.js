56dfc8f08edc3e32c1d42e875a7b9bae198fb0a2
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2023-02-28 15:25:54 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
+import './main.css';
+
+class ShopItemClass extends React.Component{
+  render() {
+    return <div className="main-content">
+      <h2>{this.props.item.brand}</h2>
+      <h1>{this.props.item.title}</h1>
+      <h3>{this.props.item.description}</h3>
+      <div className="description">
+        {this.props.item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{this.props.item.currency + this.props.item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>;
+  }
+}
 
 function App() {
+
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemClass item={item} />
+        </div>
+      </div>
   );
 }
 
